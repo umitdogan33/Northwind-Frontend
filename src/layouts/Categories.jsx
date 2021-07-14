5fce30a97100ff329bc8b3a751fabcf6b72bee6c
umit <umit9551@gmail.com>
2021-07-14 20:18:34 +0300

    product listing added

---
+++ src/layouts/Categories.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Menu } from "semantic-ui-react";
+
+export default function Categories() {
+  return (
+    <div>
+      <br></br>
+      <br></br>
+
+      <Menu pointing vertical>
+        <Menu.Item name="home" />
+        <Menu.Item name="messages" />
+        <Menu.Item name="friends" />
+      </Menu>
+    </div>
+  );
+}
